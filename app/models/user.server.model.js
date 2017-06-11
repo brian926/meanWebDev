85940231b53c84dc0cdf1c672106a9cbde044e11
brian926 <bantunes@my.ccsu.edu>
2017-06-11 04:54:28 -0400

    Added, worked with Mongoose model

---
+++ app/models/user.server.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+		website: {
+		type: String,
+		get: function(url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0 ) {
+					url = 'http://' + url;
+				}
+
+				return url;
+			}
+		}
+	},
+	firstName: String,
+	lastName: String,
+	email: {
+		type: String,
+		index: true
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true
+	},
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+UserSchema.set('toJSON', {getters: true});
+mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
 
+const db = configureMongoose();
 const app = configureExpress();
 app.listen(3000);
-module.exports = app;
 
+module.exports = app;
 console.log('Server is running at http://localhost:3000/');
