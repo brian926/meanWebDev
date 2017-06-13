8d6b3e1d334a6b777ff2defaaa562e000efef182
brian926 <bantunes@my.ccsu.edu>
2017-06-13 06:33:30 -0400

    Added user authentication

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,4 +1,5 @@
 const users = require('../../app/controllers/users.server.controller');
+const passport = require('passport');
 
 module.exports = function(app) {
 	app.route('/users').post(users.create).get(users.list);
@@ -6,4 +7,23 @@ module.exports = function(app) {
 	app.route('/users/:userId').get(users.read).put(users.update).delete(users.delete);
 
 	app.param('userId', users.userByID);
+
+	app.route('/signup').get(users.renderSignup).post(users.signup);
+
+	app.route('/signin').get(users.renderSignin).post(passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/signin',
+		failureFlash:true
+	}));
+
+	app.get('/signout', users.signout);
+
+	app.get('/oauth/facebook', passport.authenticate('facebook',{
+		failureRedirect: '/signin'
+	}));
+
+	app.get('/oauth/facebook/callback', passport.authenticate('facebook', {
+		failureRedirect: '/signin',
+		successRedirect: '/'
+	}));
 };
--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
 	db: 'mongodb://localhost/mean-book',
-	sessionSecret: 'developmentSessionSecret'
+	sessionSecret: 'developmentSessionSecret',
+	facebook: {
+		clientID: '110199792921946',
+		clientSecret: '38084f3f61167d34e373c312b3711684',
+		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+	}
 };
