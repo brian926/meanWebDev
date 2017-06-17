ec09b81d7f088e37c005133219e4c63c7e2f925e
brian926 <bantunes@my.ccsu.edu>
2017-06-17 16:34:24 -0400

    Added Angular files

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,69 +1,8 @@
 const User = require('mongoose').model('User');
 const passport =require('passport');
 
-exports.create = function(req, res, next) {
-	const user = new User(req.body);
-
-	user.save((err) => {
-		if (err) {
-			return next(err);
-		} else {
-			res.status(200).json(user);
-		}
-	});
-};
-
-exports.list = function(req, res, next) {
-	User.find({}, (err, users) => {
-		if (err) {
-			return next(err);
-		} else {
-			res.status(200).json(users);
-		}
-	});
-};
-
-exports.read = function(req, res) {
-	res.json(req.user);
-};
-
-exports.userByID = function(req, res, next, id) {
-	User.findOne({
-		_id: id
-	}, (err, user) => {
-		if (err) {
-			return next(err);
-		} else {
-			req.user = user;
-			next();
-		}
-	});
-};
-
-exports.update = function(req, res, next) {
-	User.findByIdAndUpdate(req.user.id, req.body, {
-		'new': true
-	}, (err, user) => {
-		if (err) {
-			return next(err);
-		} else {
-			res.status(200).json(user);
-		}
-	});
-};
-
-exports.delete = function(req, res, next) {
-	req.user.remove(err => {
-		if (err) {
-			return next(err);
-		} else {
-			res.status(200).json(req.user);
-		}
-	});
-};
-
 function getErrorMessage(err) {
-	let message ='';
+	const message ='';
 
 	if (err.code) {
 		switch (err.code) {
@@ -83,47 +22,49 @@ function getErrorMessage(err) {
 	return message;
 };
 
-exports.renderSignin = function(req, res, next) {
-	if (!req.user) {
-		res.render('signin', {
-			title: 'Sign-in Form',
-			messages: req.flash('error') || req.flash('info')
-		});
-	} else {
-		return res.redirect('/');
-	}
-};
+exports.signin = function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err || !user) {
+			res.status(400).send(info);
+		} else {
+			// Remove sensitive data before login
+			user.password = undefined;
+			user.salt = undefined;
 
-exports.renderSignup = function(req, res, next) {
-	if (!req.user) {
-		res.render('signup', {
-			title: 'Sign-up Form',
-			messages: req.flash('error')
-		});
-	} else {
-		return res.redirect('/');
-	}
-};
-
-exports.signup = function(req, res, next) {
-	if (!req.user) {
-		const user = new User(req.body);
-		user.provider = 'local';
-		user.save((err) => {
-			if (err) {
-				const message = getErrorMessage(err);
-
-				req.flash('error', message);
-				return res.redirect('/signup');
-			}
-			req.login(user, (err) => {
-				if (err) return next(err);
-				return res.redirect('/');
+			req.login(user, function(err) {
+				if (err) {
+					res.status(400).send(err);
+				} else {
+					res.join(user);
+				}
 			});
-		});
-	} else {
-		return res.redirect('/');
-	}
+		}
+	})(req, res, next);
+};
+
+exports.signup = function(req, res) {
+	const user = new User(req.body);
+	user.provider = 'local';
+
+	user.save((err) => {
+		if (err) {
+			return res.status(400).send({
+				message: getErrorMessage(err)
+			});
+		} else {
+			// Remove sensitive data before login
+			user.password = undefined;
+			user.salt = undefined;
+
+			req.login(user, function(err) {
+				if (err) {
+					res.status(400).send(err);
+				} else {
+					res.join(user);
+				}
+			});
+		}
+	});
 };
 
 exports.signout = function(req, res) {
@@ -142,12 +83,19 @@ exports.saveOAuthUserProfile = function(req, profile, done) {
 			if (!user) {
 				const possibleUsername = profile.username || ((profile.email) ? profile.email.split('@')[0] : '');
 
-					User.findUniqueUsername(possibleUsername, null, (availableUsername) => {
-						const newUser = new User(profile);
-						newUser.username = availableUsername;
+					User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
+						profile.username = availableUsername;
+						
+						user = new User(profile);
 
-						newUser.save((err) => {
-							return done(err, newUser);
+						user.save((err) => {
+							if (err) {
+								const message = _this.getErrorMessage(err);
+
+								req.flash('error', message);
+								return res.redirect('/signup');
+							}
+							return done(err, user);
 						});
 					});
 			} else {
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -2,27 +2,15 @@ const users = require('../../app/controllers/users.server.controller');
 const passport = require('passport');
 
 module.exports = function(app) {
-	app.route('/users').post(users.create).get(users.list);
+	app.route('api/auth/signup').post(users.signup);
+	app.route('api/auth/signin').post(users.signin);
+	app.route('api/auth/signout').get(users.signout);
 
-	app.route('/users/:userId').get(users.read).put(users.update).delete(users.delete);
-
-	app.param('userId', users.userByID);
-
-	app.route('/signup').get(users.renderSignup).post(users.signup);
-
-	app.route('/signin').get(users.renderSignin).post(passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/signin',
-		failureFlash:true
-	}));
-
-	app.get('/signout', users.signout);
-
-	app.get('/oauth/facebook', passport.authenticate('facebook',{
+	app.get('/api/oauth/facebook', passport.authenticate('facebook',{
 		failureRedirect: '/signin'
 	}));
 
-	app.get('/oauth/facebook/callback', passport.authenticate('facebook', {
+	app.get('/api/oauth/facebook/callback', passport.authenticate('facebook', {
 		failureRedirect: '/signin',
 		successRedirect: '/'
 	}));
